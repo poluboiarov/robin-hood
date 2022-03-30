e8f308fc6b808278ba8bc7529596f373f68b0f59
Evgeniy Poluboiarov <poluboiarov.dev@gmail.com>
2022-03-30 13:13:17 +0500

    Initial project files.

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="gameboard"></div>
+</template>
+
+<script>
+export default {
+  name: 'GameBoard'
+}
+</script>
+
+<style lang="less">
+</style>
---
+++ src/views/GameView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="game">
+    <GameBoard/>
+  </div>
+</template>
+
+<script>
+import GameBoard from '@/components/GameBoard.vue'
+
+export default {
+  name: 'GameView',
+  components: {
+    GameBoard
+  }
+}
+</script>
